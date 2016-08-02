fc60b29a0696ebaa5a0634de3e6f38859cd03fc2
Duncan <drdunc@gmail.com>
2016-08-02 10:33:52 +0100

    first

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key="xxx"
+consumer_secret="xxx"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token="xxx"
+access_token_secret="xxx"
---
+++ tweettest.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python2.7
+
+# working from standard yocto build on Edison
+# make sure pip is upto date on edison
+# sudo pip install --upgrade pip
+
+# download and setup tweepy in root
+# pip install tweepy
+# (note tweepy at https://github.com/tweepy/tweepy)
+
+# D Wilson June 2016 (original work 2014)
+
+
+import tweepy
+import sys
+# Consumer keys and access tokens, used for OAuth
+from settings import *
+
+
+# OAuth process, using the keys and tokens
+auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+ 
+# Creation of the actual interface, using authentication
+api = tweepy.API(auth)
+
+if len(sys.argv) >= 2:
+    tweet_text = sys.argv[1]
+
+else:
+    tweet_text = "Hello world"
+
+if len(tweet_text) <= 140:
+    api.update_status(status=tweet_text)
+else:
+    print "tweet not sent. Too long. 140 chars Max."
+
+
